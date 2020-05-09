9f741ddbadb9204f0ca4e0f2c5198758dd1a9222
Yitzik Casapu <yitzikc@gmail.com>
2020-05-09 10:40:28 +0100

    Remove MS copyright as the code is no longer a copy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,3 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
 import { WebHost } from '@microsoft/mixed-reality-extension-sdk';
 import dotenv from 'dotenv';
 import { resolve as resolvePath } from 'path';
